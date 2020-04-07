d77745b5cdbd29f2408e923f6906e7612a90fd04
Adan Gutierrez <hevyw8@gmail.com>
2020-04-07 17:25:26 -0500

    added regex, clap and file reading

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
-fn main() {
-    let search_term = "picture";
-    let quote = "Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
+extern crate clap;
+extern crate regex;
 
-    for line in quote.lines() {
-        if line.contains(search_term) {
-            println!("{}", line);
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+use clap::{App, Arg};
+use regex::Regex;
+
+fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
+
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => {}
         }
     }
 }
